b3a5071b98cdc6da24cd6e445c78eee789948d15
John Doe <johndoe@example.com>
2025-02-06 16:58:51 +0300

    НАЧАЛО

---
+++ myapp.py
@@ -0,0 +1,8 @@
+    #НАДО ПОДКЛЮЧИТЬ МОДУЛИ!!!!!!!!!
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
